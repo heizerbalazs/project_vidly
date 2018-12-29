41662ab41d2551cc07be08d77851d023d6977140
heizer.balazs <heizer.balazs@gmail.com>
2018-12-29 15:06:15 +0100

    Welcome message updated, andgeneres list added

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const app = express();
 
 app.use(express.json());
 
+const generes = [
+    { id: 1, name: 'Action' },
+    { id: 2, name: 'Sci-Fi/Fantasy' },
+    { id: 3, name: 'Comedy' }
+];
+
 app.get('/', (req, res) => {
-    res.send('Welcom on VIDLY');
+    res.send('Welcome on VIDLY');
 });
+
+app.get('/api/generes', (req, res) => {
+    res.send(generes);
+})
+
+function validateGenere(genere) {
+    const schema = {
+        name: Joi.string().min(2).required()
+    };
+    return Joi.validate(genere, schema);
+}
 
 // PORT
 const port = process.env.PORT || 3000;
