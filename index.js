4d4f917570db6eb68fb8d5562fa20627e5c45be2
heizer.balazs <heizer.balazs@gmail.com>
2018-12-29 19:33:45 +0100

    mongodb set up

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const reqresDebugger = require('debug')('vidly:reqres');
 const dbDebugger = require('debug')('vidly:db');
 const config = require('config');
@@ -7,15 +8,15 @@ const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+mongoose.connect('mongodb://localhost/vidly')
+        .then(() => dbDebugger('Connected to MongoDB...'))
+        .catch(err => dbDebugger('Could not connect to MongoDB...', err));
+
 app.use(express.json());
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/views'));
-app.use('api/genres', genres);
+app.use('/api/genres', genres);
 app.use('/', homepage);
-
-
-dbDebugger('Connected to database...');
-
 
 // PORT
 const port = process.env.PORT || 3000;
