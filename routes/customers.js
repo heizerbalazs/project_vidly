b8d9aa04771d43a98c8e35e919a074b82cd80a19
heizer.balazs <heizer.balazs@gmail.com>
2018-12-30 13:54:15 +0100

    customer api implementation is done

--- routes/customers.js
+++ routes/customers.js
@@ -27,6 +27,13 @@ router.get('/', async (req, res) => {
     res.send(customer)
 });
 
+router.get('/:id', async (req, res) => {
+    const customer = await Customer.findById(req.params.id);
+   
+    if (!customer) return res.status(404).send('The genre with the given ID was not found,');
+    res.send(customer);
+});
+
 router.post('/', async (req, res) => {
     const { error } = validateCustomer(req.body);
     if (error) return res.status(400).send(error.details[0].message);
@@ -41,7 +48,28 @@ router.post('/', async (req, res) => {
     res.send(customer);
 });
 
-// implementation of the remaining REST methods
+router.put('/:id', async (req, res) => {
+    const { error } = validateCustomer(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findByIdAndUpdate(req.params.id,
+        { name: req.body.name,
+          phone: req.body.phone,
+          isGold: req.body.isGold },
+        { new: true });
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+    res.send(customer);
+});
+
+router.delete('/:id', async (req, res) => {
+   
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+
+    if (!customer) return res.status(404).send('The customer with the given id was not found.');
+
+    res.send(customer);
+})
 
 function validateCustomer(customer) {
     const schema = {
